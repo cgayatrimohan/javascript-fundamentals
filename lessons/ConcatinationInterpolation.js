4767dba31dc0ebbb308965a9d0d833006e39f12f
Gayatri Chintakayala <c.gayatrimohan@gmail.com>
2026-09-11 11:02:13 -0700

    Updated the file name and added a new file

---
+++ lessons/ConcatinationInterpolation.js
@@ -0,0 +1,13 @@
+//Concatination and Interpolation
+var product = "Table"
+var price = 80
+var messageToPrint1 = "The price for your " + product + " is " + price + " dollars" //Concatination
+console.log(messageToPrint1)
+
+
+var messageToPrint2 = `The price for your ${product} is ${price} dollars` //Interpolation
+console.log(messageToPrint2)
+
+
+
+
