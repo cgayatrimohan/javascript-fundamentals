9409ce614a3323ac7e2684b0e56b7e269bb9c60c
Gayatri Chintakayala <c.gayatrimohan@gmail.com>
2026-09-11 10:55:30 -0700

    Added the first lesson1.js

--- lessons/lesson1.js
+++ lessons/lesson1.js
@@ -1,2 +1,28 @@
-//Hello World
-console.log("Hello world!");
+//1. Hello world
+console.log("Hello World!")
+
+//Variables
+var firstName = "John"
+let lastName = "Smith"
+console.log(firstName)
+
+var age, dateOfBirth, sex
+age = "5"
+sex = "Male"
+console.log(age)
+
+age = "6"
+console.log(age)
+
+
+//Constants
+const occupation = "Engineer"
+// occupation = "driver" // This is not possible as, it is a constant
+console.log(occupation)
+
+//data types
+var middleName = "David" //String
+var ageOfBrother = 25 //number
+var isHeMarried = false  //boolean
+var yearsInMarriage = null //null -> like no value
+var numberOfCars = undefined //undefined -> usually you will see in error states, we can't technically use this
