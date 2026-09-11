c3742947e81aa65a95fa0fbbec140aa8ee619d67
Gayatri Chintakayala <c.gayatrimohan@gmail.com>
2026-09-11 12:12:49 -0700

    Examples on class/method creation, exporting etc

--- helpers/printHelpers.js
+++ helpers/printHelpers.js
@@ -1,3 +1,23 @@
 export function printAge(age) {
     console.log(age)
 }
+
+class CustomerDetails {
+    /**
+     * This method will print firstName
+     * @param {string} firstName 
+     */
+    printFirstName(firstName) {
+        console.log(firstName)
+    }
+
+    /**
+     * This method will print the lastName
+     * @param {string} lastName 
+     */
+    printLastName(lastName) {
+        console.log(lastName)
+    }
+}
+
+export const customerDetails = new CustomerDetails()
---
+++ lessons/ClassAndMethods.js
@@ -0,0 +1,6 @@
+import { customerDetails } from "../helpers/printHelpers.js";
+
+
+//var customerDetails = new CustomerDetails() --> if the instance is not imported and the class is
+customerDetails.printFirstName("John")
+customerDetails.printLastName("Smith")
